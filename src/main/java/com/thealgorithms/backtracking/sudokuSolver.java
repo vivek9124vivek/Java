3a1946b3fef12661c9c7a428722dfbb3d99ddd6e
vivek9124vivek <103214064+vivek9124vivek@users.noreply.github.com>
2023-10-04 19:26:28 +0530

    Create sudokuSolver.java

---
+++ src/main/java/com/thealgorithms/backtracking/sudokuSolver.java
@@ -0,0 +1,89 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] sudokuGrid = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(sudokuGrid)) {
+            printSudoku(sudokuGrid);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    // Function to solve the Sudoku puzzle using backtracking
+    public static boolean solveSudoku(int[][] grid) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (grid[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(grid, row, col, num)) {
+                            grid[row][col] = num;
+                            if (solveSudoku(grid)) {
+                                return true;
+                            }
+                            grid[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true; // All cells are filled
+    }
+
+    // Function to check if it's safe to place 'num' in the given cell
+    private static boolean isSafe(int[][] grid, int row, int col, int num) {
+        return !usedInRow(grid, row, num) && !usedInCol(grid, col, num) && !usedInBox(grid, row - row % 3, col - col % 3, num);
+    }
+
+    // Function to check if 'num' is used in the given row
+    private static boolean usedInRow(int[][] grid, int row, int num) {
+        for (int col = 0; col < 9; col++) {
+            if (grid[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Function to check if 'num' is used in the given column
+    private static boolean usedInCol(int[][] grid, int col, int num) {
+        for (int row = 0; row < 9; row++) {
+            if (grid[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Function to check if 'num' is used in the 3x3 box starting at (row, col)
+    private static boolean usedInBox(int[][] grid, int row, int col, int num) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[row + i][col + j] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Function to print the solved Sudoku grid
+    public static void printSudoku(int[][] grid) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(grid[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
